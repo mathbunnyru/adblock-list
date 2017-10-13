70fa51f8ad492debf02a40c20225ff27f75b80cd
Ayaz Salikhov <mathbunnyru@gmail.com>
2017-10-13 21:58:49 +0300

    Update script

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
 #!/usr/bin/env python
 
+
 def simplify(filename):
-	with open(filename) as file:
-		lines = file.read().split('\n')
+    lines = open(filename).read().split('\n')
 
-	clean_lines = []
-	for line in lines:
-		if 'class' in line and '#' in line:
-			website = line[:line.find('#')]
-			class_names = line[line.find('"') + 1 : line.find(']') - 1].split()
-			for name in class_names:
-				clean_lines.append(website + '##.' + name)
-		else:
-			clean_lines.append(line)
+    clean_lines = []
+    for line in lines:
+        if 'class' in line and '#' in line:
+            website = line[:line.find('#')]
+            class_names = line[line.find('"') + 1: line.find(']') - 1].split()
+            class_names.extend(website + '##.' + name for name in class_names)
+        else:
+            clean_lines.append(line)
 
-	with open(filename, "w") as file:
-		adblock_header = clean_lines[:8]
-		websites = sorted(set(clean_lines[8:]))
-		file.write('\n'.join(adblock_header + websites))
+    with open(filename, "w") as file:
+        adblock_header = clean_lines[:8]
+        websites = sorted(set(clean_lines[8:]))
+        file.write('\n'.join(adblock_header + websites) + '\n')
 
 
 simplify('block-list.txt')
